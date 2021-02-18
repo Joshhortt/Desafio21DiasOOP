2db0085a49acf8d3b6a7aeaa41a25677c9d0351b
Joshhortt <joshhortt@yahoo.com>
2021-02-18 19:18:56 +0000

    Adicionar a aula 02 do Desafio21Dias 'Classes e Construtores' em POO.

---
+++ 02-CriandoClassesConstrutores/Desafio21DiasOO/Desafio21DiasOO/Pessoa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alunos
+{
+	public class Pessoa
+	{
+		public Pessoa()  // Cosntrutor
+		{
+		}
+
+		public int Id { get; set; }
+		public string Nome { get; set; }
+		public string Endereço { get; set; }
+		public string Cpf { get; set; }
+	}
+}
---
+++ 02-CriandoClassesConstrutores/Desafio21DiasOO/Desafio21DiasOO/Program.cs
@@ -0,0 +1,45 @@
+﻿using Alunos;
+using System;
+using System.Collections.Generic;
+
+namespace Alunos
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Pessoa jose = new Pessoa();    // criando uma nova instancia atraves do modelo. Tipagem estatica
+			// caracteristicas do objeto.
+			jose.Id = 1;                   // ao escrver 'jose.' aparecem as variaveis dentro do objeto jose disponiveis.
+			jose.Nome = "Joshhortt";
+			jose.Endereço = "Rua D";
+			jose.Cpf = "096905463";
+
+			Pessoa wagner = new Pessoa();
+			wagner.Id = 2;                   // 
+			wagner.Nome = "waggi";
+			wagner.Endereço = "Rua B";
+			wagner.Cpf = "096905461";
+
+			Pessoa marcos = new Pessoa();
+			marcos.Id = 3;                   // 
+			marcos.Nome = "marcci";
+			marcos.Endereço = "Rua C";
+			marcos.Cpf = "096905462";
+
+			var suzi = new Pessoa();  // usando o 'var' em vez de 'Pessoa' tb podemos. Chama se tipagem dinamica.
+			suzi.Id = 4;                   // 
+			suzi.Nome = "suazii";
+			suzi.Endereço = "Rua C";
+			suzi.Cpf = "096905462";
+
+			List<Pessoa> pessoas = new List<Pessoa>(); // Criando uma nova Lista de Pessoa onde colocaria todas as Pessoas
+
+			pessoas.Add(jose);
+			pessoas.Add(wagner);
+			pessoas.Add(marcos);
+
+			Console.WriteLine("Olá 2ª aula do Desafio 21 dias de POO!");
+		}
+	}
+}
