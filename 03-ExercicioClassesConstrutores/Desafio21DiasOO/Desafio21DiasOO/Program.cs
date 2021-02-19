14a25a5d16d8f10b51ea341120f6289ab593d6fd
Joshhortt <joshhortt@yahoo.com>
2021-02-19 12:59:57 +0000

    Adicionar Exercicio do dia 3. Criar classe Receita e Construtores com metodos e propriedades.

---
+++ 03-ExercicioClassesConstrutores/Desafio21DiasOO/Desafio21DiasOO/Program.cs
@@ -0,0 +1,39 @@
+﻿
+//03. Exercicio: Exercitando Classes e Construtores
+// Criar uma Instancia de uma classe chamada Bolo.
+// A classe Receita tem de ter 4 Propriedades (Peso, tamanho, preço e nome.)
+// A classe Receita tem de ter 2 Metodos (Salvar() e Mostrar().
+
+using System;
+
+namespace Desafio21DiasOO
+
+	{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Receita boloChocolate = new Receita();
+
+            boloChocolate.Nome = "Bolo de chocolate";
+            boloChocolate.Tamanho = "Pequeno";
+            boloChocolate.Peso = 1.2F;  // Float
+         // boloChocolate.Peso = 1.2;  Double
+            boloChocolate.Preco = 35.00F;
+
+            Receita boloLaranja = new Receita();
+            boloLaranja.Nome = "Bolo Laranja";
+            boloLaranja.Peso = 1.4F;  // Float
+         // boloLaranja.Peso = 1.4; Double
+            boloLaranja.Tamanho = "Medio";
+            boloLaranja.Preco = 40.00F;
+
+            Receita boloMorango = new Receita();
+            boloMorango.Nome = "Bolo Morango";
+            boloMorango.Peso = 1.7F;  // Float
+        //  boloMorango.Peso = 1.7;  Double
+            boloMorango.Tamanho = "Grande";
+            boloMorango.Preco = 45.00F;
+        }
+    }
+}
---
+++ 03-ExercicioClassesConstrutores/Desafio21DiasOO/Desafio21DiasOO/Receita.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Desafio21DiasOO
+{
+    public class Receita  // classe Receita
+    {
+        public Receita(string nome = "")  // construtor com parametro
+            {
+            this.Nome = nome;
+            }
+
+        public Receita() { } // construtor vazio (sem parametros);
+
+        // propriedades
+        public string Nome { get; set; }
+        public string Tamanho { get; set; }
+        public float Peso { get; set; }
+       // public double Peso { get; set; }
+        public float Preco { get; set; }
+       // public double Preco { get; set; }
+
+
+        // metodo Salvar
+        public void Salvar(Receita receita)
+        {
+			Console.WriteLine("Está salvo!");
+        }
+
+        // metodo Mostrar
+        public void Mostrar()
+        {
+
+            
+        }
+    }
+}
