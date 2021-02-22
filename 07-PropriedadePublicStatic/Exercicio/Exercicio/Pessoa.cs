65452abe6bb023682dbd5cef0107229054d37368
Joshhortt <joshhortt@yahoo.com>
2021-02-22 16:47:52 +0000

    Adicionar Exercicio 7 ao desafio 21Dias. 3 classes 'Pessoa','Fisica', 'juridica' com propriedade 'static 'com Herança e Polimorfismo'.

---
+++ 07-PropriedadePublicStatic/Exercicio/Exercicio/Pessoa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public string Endereco { get; set; }
+
+        protected string NomePersonalizadoProtected()
+        {
+            return $"\nOla {Nome} eu sou o metodo Protected:\n";
+        }
+        public virtual string NomePersonalizadoVirtual()
+        {
+            return $"\nOla {Nome} eu sou o Metodo Virtual:\n";
+        }
+
+    }
+}
---
+++ 07-PropriedadePublicStatic/Exercicio/Exercicio/Program.cs
@@ -0,0 +1,19 @@
+﻿// 07. Propriedade Publica e Estatica (continuação da Herança e Polimorfismo das aulas anteriores)
+
+using System;
+
+namespace Exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fisica usuario = new Fisica("Jose");
+            Console.WriteLine(usuario.Nome);
+            Console.WriteLine(usuario.NomePersonalizadoProtected());
+            Console.WriteLine(usuario.NomePersonalizadoVirtual());
+
+            System.Console.ReadKey();
+        }
+    }
+}
