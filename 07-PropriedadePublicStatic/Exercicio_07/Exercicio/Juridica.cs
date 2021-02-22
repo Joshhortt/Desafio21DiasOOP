c7b161357a725d35a15be9142551ef93191fdf2c
Joshhortt <joshhortt@yahoo.com>
2021-02-22 19:00:46 +0000

    Adicionar Exercicio TPC 7 ao desafio 21Dias, sobre propriedades 'static 'com Herança e Polimorfismo'.

---
+++ 07-PropriedadePublicStatic/Exercicio_07/Exercicio/Juridica.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio
+{
+    public class Juridica : Pessoa
+    {
+        //public string Cnpj { get; set; }
+        //public string NomeFantasia { get; set; }
+
+        //public static List<Juridica> Base = new List<Juridica>();
+        //public void Salvar()
+        //{
+        //    Juridica.Base.Add(this);
+        //}
+
+        //public static List<Juridica> BuscaPorNome(string nome)
+        //{
+        //    var novaLista = new List<Juridica>();
+        //    foreach (var instancia in Juridica.Base)
+        //    {
+        //        if (instancia.Nome.ToLower().Contains(nome.ToLower()))
+        //        {
+        //            novaLista.Add(instancia);
+        //        }
+        //    }
+        //    return novaLista;
+        //}
+    }
+}
---
+++ 07-PropriedadePublicStatic/Exercicio_07/Exercicio/Pessoa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio
+{
+    public class Pessoa
+    {
+        public Pessoa()
+        {
+
+        }
+        public string Nome { get; set; }
+        public string Endereco { get; set; }
+        public virtual string NomePersonalizado() { return $"{this.Nome} - Aula torne-se um programador"; }
+    }
+} 
